1c791c420550aa52dbe71aad137fb782a29f8d4e
gftang <gftang@vcainfo.com>
2016-05-30 17:51:15 +0800

    bug fix

--- MyLibrary/Classes/BlinkingLabel.swift
+++ MyLibrary/Classes/BlinkingLabel.swift
@@ -7,7 +7,7 @@
 //
 public class BlinkingLabel: UILabel {
     public func startBlinking() {
-        let options : UIViewAnimationOptions = .Repeat | .Autoreverse
+        let options : UIViewAnimationOptions = .Repeat
         UIView.animateWithDuration(0.25, delay:0.0, options:options, animations: {
             self.alpha = 0
             }, completion: nil)
