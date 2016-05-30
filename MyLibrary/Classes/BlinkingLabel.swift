0dea1752e7d882b2a2074f5fe9ef4735d61df11a
gftang <gftang@vcainfo.com>
2016-05-30 17:49:22 +0800

    add label class

---
+++ MyLibrary/Classes/BlinkingLabel.swift
@@ -0,0 +1,20 @@
+//
+//  BlinkingLabel.swift
+//  Pods
+//
+//  Created by VcaiTech on 16/5/30.
+//
+//
+public class BlinkingLabel: UILabel {
+    public func startBlinking() {
+        let options : UIViewAnimationOptions = .Repeat | .Autoreverse
+        UIView.animateWithDuration(0.25, delay:0.0, options:options, animations: {
+            self.alpha = 0
+            }, completion: nil)
+    }
+    
+    public func stopBlinking() {
+        alpha = 1
+        layer.removeAllAnimations()
+    }
+}
